d0adde45641593394f06669d09684d03b0637073
Renan Arantes <renanpietro360@gmail.com>
2026-07-01 12:07:12 -0300

    Organiza ordem final da home

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,31 @@
-import FAQ from "../components/sections/FAQ";
-import Testimonials from "../components/sections/Testimonials";
-import Clients from "@/components/sections/Clients";
-import WhatsAppButton from "../components/sections/WhatsAppButton";
 import Navbar from "../components/sections/Navbar";
 import Hero from "../components/sections/Hero";
+import Clients from "../components/sections/Clients";
 import About from "../components/sections/About";
 import Services from "../components/sections/Services";
 import Portfolio from "../components/sections/Portfolio";
 import Process from "../components/sections/Process";
+import Testimonials from "../components/sections/Testimonials";
+import FAQ from "../components/sections/FAQ";
 import CTA from "../components/sections/CTA";
 import Footer from "../components/sections/Footer";
+import WhatsAppButton from "../components/sections/WhatsAppButton";
 
 export default function Home() {
   return (
-   <>
-  <Navbar />
-  <Hero />
-  <Clients />
-  <About />
-  <Services />
-  <Portfolio />
-  <Process />
-  <Testimonials />
-  <FAQ />
-  <CTA />
-  <Footer />
-  <WhatsAppButton />
-</>
+    <>
+      <Navbar />
+      <Hero />
+      <Clients />
+      <About />
+      <Services />
+      <Portfolio />
+      <Process />
+      <Testimonials />
+      <FAQ />
+      <CTA />
+      <Footer />
+      <WhatsAppButton />
+    </>
   );
 }
